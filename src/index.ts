2859e26737971e2d767c46aa329c2df4fe4b4e71
Fadoon T <fadoon.tar@gmail.com>
2024-06-12 09:59:38 +0100

    added 404

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use("/api/todo", todoRoute);
 app.use("/api/auth", authRoute);
 
-
+app.get("*", (req: express.Request, res: express.Response) => {
+  res.status(404).send("Route does not exist");
+});
 
 app.use(
   (
@@ -27,7 +29,6 @@ app.use(
     res.status(500).json({ message: err.message });
   }
 );
-
 
 // Test database connection and sync models
 sequelize
